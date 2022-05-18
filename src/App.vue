7aed631b7e62a985fddd23bec282eb24cdfd0f61
Simone Sada <si.sada93@gmail.com>
2022-05-18 17:31:04 +0200

    Add scss files

--- src/App.vue
+++ src/App.vue
@@ -25,13 +25,9 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+@import './assets/scss/_variables.scss';
+@import './assets/scss/_mixins.scss';
+@import './assets/scss/style.scss';
+
 </style>
